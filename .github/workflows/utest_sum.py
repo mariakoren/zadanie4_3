0f41aae6e5b9184845e58a8b449298c7b7c18194
mariakoren <116657002+mariakoren@users.noreply.github.com>
2022-11-06 11:17:51 +0100

    Update utest_sum.py

--- .github/workflows/utest_sum.py
+++ .github/workflows/utest_sum.py
@@ -4,7 +4,7 @@ import suma
 
 class SumTest(unittest.TestCase):
     def test_sumowanie(self):
-        self.assertEqual(suma.sumowanie(1,2), 3)  # add assertion here
+        self.assertEqual(suma.sumowanie(2,2), 4)  # add assertion here
 
 
 if __name__ == '__main__':
